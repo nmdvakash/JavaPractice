f184f28b79baffca4568e336e96f35fd10b9afe5
nmdvakash <nmdv.akash@gmail.com>
2020-02-11 01:00:34 +0530

    Made some changes

--- src/com/string/LongestSubString.java
+++ src/com/string/LongestSubString.java
@@ -7,38 +7,40 @@ import java.util.TreeMap;
 
 public class LongestSubString {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String str = "thelongestsubstring";
-		System.out.println(" --- :: " + getLongestSubString(str));
+        String str = "thelongestsubstring";
+        Map.Entry<Integer, String> entry = getLongestSubString(str);
 
-	}
+        // find the longest non repeatable char sub string
+        System.out.println("Out put Longest Sub String  :: " + entry.getValue());
+        //Longest Sub String  :: [u, b, s, t, r, i, n, g]
 
-	public static Integer getLongestSubString(String str) {
+        System.out.println("Max length --- :: " + entry.getKey());
+        // Output --- :: 8
+    }
 
-		TreeMap<Integer, String> map = new TreeMap<>();
-		List<String> list = new ArrayList<>();
-		int j = 0;
-		for (int i = 0; i < str.length(); i++) {
-			if (j == 0) {
-				j = i;
-			}
-			if (!list.contains(str.charAt(i) + "")) {
-				list.add(str.charAt(i) + "");
-			} else {
-				map.put(list.size(), list.toString());
-				list.clear();
-				i = j;
-				j = 0;
-			}
-		}
-		map.put(list.size(), list.toString());
-		map.keySet().stream().forEach(System.out::println);
+    public static Map.Entry<Integer, String> getLongestSubString(String str) {
+        TreeMap<Integer, String> map = new TreeMap<>();
+        List<String> list = new ArrayList<>();
+        int j = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (j == 0) {
+                j = i;
+            }
+            if (!list.contains(str.charAt(i) + "")) {
+                list.add(str.charAt(i) + "");
+            } else {
+                map.put(list.size(), list.toString());
+                list.clear();
+                i = j;
+                j = 0;
+            }
+        }
+        map.put(list.size(), list.toString());
 //		List<Integer> test = map.values().stream().sorted(Comparator.<Integer>reverseOrder())
 //				.collect(Collectors.toList());
-
-		return map.lastEntry().getKey();
-
-	}
+        return map.lastEntry();
+    }
 
 }
