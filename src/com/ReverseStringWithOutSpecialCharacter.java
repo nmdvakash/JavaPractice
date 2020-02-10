afba19a6894669df004f3b150a11eaba1c04cc52
nmdvakash <nmdv.akash@gmail.com>
2020-02-11 00:43:19 +0530

    Made some changes

--- src/com/ReverseStringWithOutSpecialCharacter.java
+++ src/com/ReverseStringWithOutSpecialCharacter.java
@@ -2,29 +2,30 @@ package com;
 
 public class ReverseStringWithOutSpecialCharacter {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        String str = "a#b%1C$";
+        String output = getReverseStringWithoutSpecialCharacter(str);
+        System.out.println("output  ::  " + output);
+        // output  ::  C#b%1a$
+    }
 
-		String str = "a#b%1C$";
-		int len = str.length() - 1, i = 0;
-
-		char[] chars = str.toCharArray();
-
-		while (len > i) {
-
-			if (!Character.isAlphabetic(str.charAt(i)))
-				i++;
-			else if (!Character.isAlphabetic(str.charAt(len))) {
-				len--;
-			} else {
-				char tmp = chars[i];
-				chars[i] = chars[len];
-				chars[len] = tmp;
-				len--;
-				i++;
-			}
-		}
-
-		System.out.println("out put  ::  " + new String(chars));
-
-	}
+    public static String getReverseStringWithoutSpecialCharacter(String str) {
+        int len = str.length() - 1;
+        int i = 0;
+        char[] chars = str.toCharArray();
+        while (len > i) {
+            if (!Character.isAlphabetic(str.charAt(i)))
+                i++;
+            else if (!Character.isAlphabetic(str.charAt(len))) {
+                len--;
+            } else {
+                char tmp = chars[i];
+                chars[i] = chars[len];
+                chars[len] = tmp;
+                len--;
+                i++;
+            }
+        }
+        return String.valueOf(chars);
+    }
 }
