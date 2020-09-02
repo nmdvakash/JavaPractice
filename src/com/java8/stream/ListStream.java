0f7e3eaa8e6234958430fdf16bb93fd83b7b7422
Apeksha namdeo <69234856+namdeoapeksha01@users.noreply.github.com>
2020-09-02 16:27:15 +0530

    List Operation on number updated

--- src/com/java8/stream/ListStream.java
+++ src/com/java8/stream/ListStream.java
@@ -1,6 +1,5 @@
 package com.java8.stream;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -13,25 +12,29 @@ public class ListStream {
 
         int min = 0;
         int max = 1_0;
+
+        //will create list of Integer number Output 0 to 9
         List<Integer> numberList = IntStream.range(min, max).boxed().collect(Collectors.toList());
         numberList.stream().forEach(System.out::println);
 
+        //Sum of 1 to 100 number with stream will print 5050
         int sum = IntStream.rangeClosed(1, 100).sum();
         System.out.println("value ---------- :: " + sum);
         System.out.println("----------------- X -----------------");
 
-        List<Integer> numberList2 = IntStream.rangeClosed(min, max).boxed().collect(Collectors.toList());
-        numberList2.stream().forEach(System.out::println);
+        //will create List of Integer number Output will be 0 to 10
+        numberList = IntStream.rangeClosed(min, max).boxed().collect(Collectors.toList());
+        numberList.stream().forEach(System.out::println);
 
-        int sum1 = numberList.stream().reduce(Integer::sum).get();
-        System.out.println(" sum of reduce ------ :: " + sum1);
+        sum = numberList.stream().reduce(Integer::sum).get();
+        int mapToIntSum = numberList.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(" sum of reduce ------ :: " + sum);
+        System.out.println(" mapToIntSum ------ :: " + mapToIntSum);
 
+        //find the Max number from list
         numberList.parallelStream().reduce(Integer::max).stream().forEach(System.out::println);
 
-
         List<Integer> list = Arrays.asList(2, 3, 43, 2, 4, 5, 63, 2, 4, 6);
-        int sumTest = list.stream().mapToInt(Integer::intValue).sum();
-        System.out.println("Sum of list :: " + sumTest);
         // sort the list
         System.out.println("---- sort --- ");
         list.stream().sorted().forEach(System.out::print);
@@ -40,12 +43,11 @@ public class ListStream {
         list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
 
         System.out.println("---- remove duplicates --- ");
-        // Remove duplicates
         list.stream().distinct().forEach(System.out::print);
 
-        System.out.println("out -----");
-        list.stream().distinct().peek(System.out::println).collect(Collectors.toList());
-
+        //Print steam with out for loop
+        list = list.stream().distinct().peek(System.out::println).collect(Collectors.toList());
+        System.out.println("out ----- :: "+list);
 
     }
 }
